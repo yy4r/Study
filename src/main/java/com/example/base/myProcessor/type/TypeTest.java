bff024dd7b69aad77c632eea63360f4b77d23710
zengjunjie <>
2020-12-02 14:26:17 +0800

    注解测试

--- src/main/java/com/example/base/myProcessor/type/TypeTest.java
+++ src/main/java/com/example/base/myProcessor/type/TypeTest.java
@@ -3,7 +3,11 @@ package com.example.base.myProcessor.type;
 import com.example.base.myProcessor.annotation.NodeType;
 import lombok.Data;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.xml.transform.Source;
@@ -11,14 +15,18 @@ import javax.xml.transform.Source;
 @Component
 @Data
 @NodeType(type = "type",group = "group")
-public class TypeTest implements BeanPostProcessor {
+public class TypeTest implements BeanPostProcessor, ApplicationContextAware {
 
     private String type;
 
     private String group;
 
+    private ApplicationContext applicationContext;
+
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Environment environment = applicationContext.getEnvironment();
+        String aaaa = environment.getProperty("aaaa");
         if(beanName.equals("typeTest")){
             System.out.println(11111);
         }
@@ -33,5 +41,10 @@ public class TypeTest implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         return bean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
