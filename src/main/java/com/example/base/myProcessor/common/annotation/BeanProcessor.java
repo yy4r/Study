831855bce32b7f2c038b8fe7add4452b5af8c6e5
zengjunjie <>
2021-03-01 20:09:39 +0800

    postProcessor修改

--- src/main/java/com/example/base/myProcessor/common/annotation/BeanProcessor.java
+++ src/main/java/com/example/base/myProcessor/common/annotation/BeanProcessor.java
@@ -1,11 +1,14 @@
 package com.example.base.myProcessor.common.annotation;
 
 import com.example.base.myProcessor.node.custom.CustomProcessNode;
+import lombok.SneakyThrows;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
 
 @Component
 public class BeanProcessor implements BeanPostProcessor, ApplicationContextAware {
@@ -19,6 +22,7 @@ public class BeanProcessor implements BeanPostProcessor, ApplicationContextAware
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         /**
          * 后续可考虑从配置中心拉 做动态解耦
+         * 和spring解析注解一样，注解解析之后需要放进代理类
          */
 //        Environment environment = applicationContext.getEnvironment();
 //        String aaaa = environment.getProperty("aaaa");
@@ -29,8 +33,17 @@ public class BeanProcessor implements BeanPostProcessor, ApplicationContextAware
         return bean;
     }
 
+    @SneakyThrows
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean.getClass().isAnnotationPresent(ProcessType.class)) {
+            Class<?> clazz = bean.getClass();
+            Field field = clazz.getDeclaredField("value");
+            if (field!=null){
+                Object o = field.get(bean);
+                System.out.println(o.toString());
+            }
+        }
         return bean;
     }
 
